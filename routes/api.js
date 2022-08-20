5c5a04440e71ebf40b133046aacca8b9eafd41ee
Jabarre Jarrett <jabarrejarrett@jabarres-air.attlocal.net>
2022-08-20 15:13:33 -0500

    turned on autosave

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,31 @@
-"use strict";
-const express = require("express");
-const expect = require("chai").expect;
-const ConvertHandler = require("../controllers/convertHandler.js");
+'use strict';
 
-let convertHandler = new ConvertHandler();
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function getRoutes() {
-  const router = express.Router();
-  //routes will go here with there function
-  router.get("/convert", getMetricConverter);
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
 
-  return router;
-}; 
+  app.route('/api/convert')
+    .get(function(req, res) {
+      let input = req.query.input;
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
 
-function getMetricConverter(req, res) {
-  let input = req.query.input;
-  let initNum = convertHandler.getNum(input);
-  let initUnit = convertHandler.getUnit(input);
+      if(!initNum && !initUnit){
+        res.send("invalid number and unit");
+      } else if(!initNum) {
+        res.send("invalid number");
+      } else if(!initUnit) {
+        res.send("invalid unit");
+      }
 
-  if (!initNum && !initUnit) {
-    res.send("invalid number and unit");
-  } else if (!initNum) {
-    res.send("invalid number");
-  } else if (!initUnit) {
-    res.send("invalid unit");
-  }
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-  let returnNum = convertHandler.convert(initNum, initUnit)
-  let returnUnit = convertHandler.getReturnUnit(initUnit)
-  let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+      res.json({ initNum, initUnit, returnNum, returnUnit, string: toString })
+    })
 
-  res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
-}
+};
